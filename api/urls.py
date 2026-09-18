2a4f11016a541c31b24e41a60e00d621ae2f56a0
gesicht <victorwachira100@gmail.com>
2026-09-18 13:28:48 +0300

    revamp 2.0

--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,14 @@
-
 from django.urls import path
-from .views import Index,BulletingDetailView, IntelDetailView, IntelNotes, Lboard
+from .views import Index, Writeups, WriteUpDetailView, IntelDetailView, IntelNotes, Lboard
 from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    #path('api/var/log', BulletinsListView.as_view(), name='bulletins'),
-    path('bulletin/<uuid:pk>/',BulletingDetailView.as_view(), name='bulletin'),
-    path('intel/<uuid:pk>/',IntelDetailView.as_view(), name='intel'),
-    path('ID/',IntelNotes.as_view(), name='id'),
-    path('L-board', Lboard.as_view(),name='LBoard'),
-    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
-    ),
-
-    #path('extract-cv/', Extract-cv.as_view(), name='extract-cv')
-
+    path('writeups/', Writeups.as_view(), name='writeups'),
+    path('writeup/<uuid:pk>/', WriteUpDetailView.as_view(), name='writedetail'),
+    path('intel/<uuid:pk>/', IntelDetailView.as_view(), name='intel'),
+    path('ID/', IntelNotes.as_view(), name='id'),
+    path('L-board', Lboard.as_view(), name='LBoard'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
